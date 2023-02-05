24385217180998b80d17a54f7365008c0ceb52a6
razak <mohammedrazak2001@gmail.com>
2023-02-05 13:13:18 +0530

    add Result.java

--- src/main/java/com/example/springrest/entities/Entity.java
+++ src/main/java/com/example/springrest/entities/Entity.java
@@ -1,2 +1,26 @@
-package com.example.springrest.entities;public class Entity {
+package com.example.springrest.entities;
+
+public class Entity {
+    private String name;
+
+    public Entity(String name) {
+        this.name = name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String name() {
+        return this.name;
+    }
+    public static Entity createEntity(String name) {
+        return switch (name) {
+            case "rock" -> new Rock(name);
+            case "paper" -> new Paper(name);
+            case "scissors" -> new Scissors(name);
+            default -> throw new IllegalArgumentException("Invalid entity name: " + name);
+        };
+    }
+    public boolean beats(Entity other) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
--- src/main/java/com/example/springrest/entities/Result.java
+++ src/main/java/com/example/springrest/entities/Result.java
@@ -1,2 +1,17 @@
-package com.example.springrest.entities;public class Response {
+package com.example.springrest.entities;
+
+public class Result {
+
+
+
+    public static String getResult(Entity user, Entity computer) {
+        if(user.name().equals(computer.name())) {
+            return "It is a tie!";
+        } else if (user.beats(computer)) {
+            return "Player wins!";
+        } else if (computer.beats(user)){
+            return "Computer wins!";
+        }
+        throw new IllegalArgumentException("Invalid entity name/names");
+    }
 }
